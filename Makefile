e931bca1884c53d681659ca81c5bc30621495c26
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-16 23:50:57 +0900

    [fix]

--- Makefile
+++ Makefile
@@ -6,3 +6,9 @@ DOCKER_COMPOSE := user=$(USER) docker-compose
 
 up:
 	$(DOCKER_COMPOSE) up -d
+
+shRust:
+	$(DOCKER_COMPOSE) exec api bash
+
+shDb:
+	$(DOCKER_COMPOSE) exec postgis bash
--- migrations/20211016141752_create_questions.up.sql
+++ migrations/20211016141752_create_questions.up.sql
@@ -1,17 +1,21 @@
 -- Add up migration script here
 CREATE TABLE questions (
     id UUID PRIMARY KEY,
-    title VARCHAR(255) NOT NULL INDEX,
+    title VARCHAR(255) NOT NULL,
     text TEXT,
     longitude NUMERIC(10,7) NOT NULL,
     latitude NUMERIC(9,7) NOT NULL,
     location_point geography(POINT, 4326) NOT NULL,
     address_id UUID,
-    user_id UUID NOT NULL INDEX,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP INDEX,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY user_id REFERENCES users(id),
-    FOREIGN KEY address_id REFERENCES addresses(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
 
 CREATE INDEX questions_location_point_index on questions USING GIST (location_point);
+CREATE INDEX questions_title_index on questions(title);
+CREATE INDEX questions_created_at_index on questions(created_at);
+CREATE INDEX questions_user_id_index on questions(user_id);
+CREATE INDEX questions_address_id_index on questions(address_id);
