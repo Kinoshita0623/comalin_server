0acfad50e7c87bd42cc9bcb6256317fda9916c9b
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-18 19:39:34 +0900

    change

--- migrations/20211016132319_create_users.up.sql
+++ migrations/20211016132319_create_users.up.sql
@@ -2,6 +2,7 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     username VARCHAR(255) UNIQUE,
+    avatar_icon VARCHAR(255),
     encrypted_password VARCHAR(255),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+mod users;
+
+use users::entities::User;
+use sqlx;
+use sqlx::types::Uuid;
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let connection = "postgres://dbuser:secret@postgis:5432/database";
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(20)
+        .connect(connection)
+        .await?;
+
+    let users = sqlx::query_as::<_, User>("select * from users")
+        .fetch_all(&pool)
+        .await?;
+
+    println!("{:?}", users.len());
+
+    let mut tr = pool.begin().await?;
+    sqlx::query!(
+        r#"
+        INSERT INTO users(
+            id,
+            username,
+            encrypted_password,
+            avatar_icon
+        ) VALUES (
+            $1,$2,$3,$4
+        )
+        "#,
+        Uuid::new_v4(),
+        "hogehawefrsawfrgsgeoge",
+        "piyo",
+        "fijfijfi"
+    )
+    .execute(&mut tr)
+    .await?;
+    
+    tr.commit().await?;
+
+    
+    Ok(())
 }
