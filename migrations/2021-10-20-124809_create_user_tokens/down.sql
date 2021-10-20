4ff082fe43f20169fb633c8c5828278d54cb80b0
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-20 22:03:36 +0900

    実装しました

--- migrations/2021-10-20-124809_create_user_tokens/down.sql
+++ migrations/2021-10-20-124809_create_user_tokens/down.sql
@@ -1,1 +1,3 @@
 -- This file should undo anything in `up.sql`
+DROP TABLE user_tokens;
+DROP INDEX user_tokens_hashed_token_index;
--- migrations/2021-10-20-124809_create_user_tokens/up.sql
+++ migrations/2021-10-20-124809_create_user_tokens/up.sql
@@ -1,1 +1,8 @@
 -- Your SQL goes here
+CREATE TABLE user_tokens (
+    id UUID PRIMARY KEY,
+    hashed_token VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX user_tokens_hashed_token_index ON user_tokens(hashed_token);
