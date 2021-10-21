a42b8c72f9c89db821d609829f4ec221fa2944cc
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-22 02:09:48 +0900

    [change]

--- src/user/repositories.rs
+++ src/user/repositories.rs
@@ -1,11 +1,11 @@
 use uuid::Uuid;
+use crate::errors::service_error::ServiceError;
 use crate::user::entities::{NewUser, User};
-use anyhow::Result;
 
 
 pub trait UserRepository {
-    fn create(&self, user: &NewUser) -> Result<User>;
-    fn find_by_name(&self, username: String) -> Result<User>;
-    fn find(&self, id: Uuid) -> Result<User>;
-    fn delete(&self, id: Uuid) -> Result<bool>;
+    fn create(&self, user: &NewUser) -> Result<User, ServiceError>;
+    fn find_by_name(&self, username: String) -> Result<User, ServiceError>;
+    fn find(&self, id: Uuid) -> Result<User, ServiceError>;
+    fn delete(&self, id: Uuid) -> Result<bool, ServiceError>;
 }
