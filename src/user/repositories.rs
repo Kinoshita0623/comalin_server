858b1c77690b096fb9192684da61a15ffee58b54
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-22 11:39:47 +0900

    [change] add method

--- src/user/repositories.rs
+++ src/user/repositories.rs
@@ -1,6 +1,6 @@
 use uuid::Uuid;
 use crate::errors::service_error::ServiceError;
-use crate::user::commands::NewUser;
+use crate::user::commands::{NewUser, NewUserToken};
 use crate::user::entities::User;
 
 
@@ -9,4 +9,7 @@ pub trait UserRepository {
     fn find_by_name(&self, username: String) -> Result<User, ServiceError>;
     fn find(&self, id: Uuid) -> Result<User, ServiceError>;
     fn delete(&self, id: Uuid) -> Result<bool, ServiceError>;
+    fn find_by_token(&self, token: &str) -> Result<User, ServiceError>;
+    fn save_token(&self, token: NewUserToken) -> Result<(), ServiceError>;
+    fn delete_token(&self, token: &str) -> Result<(), ServiceError>;
 }
