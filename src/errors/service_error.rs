d0f9a3f0883847a983dd8c35ef6b366c7aba9d14
kinoshita <kinoshita062323@gmail.com>
2021-10-24 03:07:17 +0900

    [change] エラーをHTTPレスポンスへ返還する処理を実装しました

--- src/errors/service_error.rs
+++ src/errors/service_error.rs
@@ -1,4 +1,9 @@
 use validator::ValidationErrors;
+use actix_web::HttpResponse;
+use serde::Serialize;
+use actix_web::http::StatusCode;
+
+#[derive(Debug)]
 pub enum ServiceError {
     AuthenticationError,
     ValidationError {
@@ -9,4 +14,50 @@ pub enum ServiceError {
     },
     NotFoundError
 
+}
+
+#[derive(Serialize)]
+struct ErrorBody<T: Serialize> {
+    message: String,
+    body: T 
+}
+
+impl ServiceError {
+
+    pub fn response(&self) -> HttpResponse {
+        return match &self {
+            ServiceError::AuthenticationError => {
+                HttpResponse::build(StatusCode::UNAUTHORIZED).json(
+                    ErrorBody {
+                        message: "Unauthorized".to_string(),
+                        body: "未認証状態又はTokenが無効です。"
+                    }
+                )
+            }
+            ServiceError::ValidationError { body } => {
+                HttpResponse::build(StatusCode::BAD_REQUEST).json(
+                    ErrorBody {
+                        message: "Validation Failed".to_string(),
+                        body: body
+                    }
+                )
+            }
+            ServiceError::InternalError { body: _ } => {
+                HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR).json(
+                    ErrorBody {
+                        message: "Server Error".to_string(),
+                        body: "サーバで問題が発生しました。管理者に連絡してください".to_string()
+                    }
+                )
+            }
+            ServiceError::NotFoundError => {
+                HttpResponse::build(StatusCode::NOT_FOUND).json(
+                    ErrorBody {
+                        message: "Not Found".to_string(),
+                        body: "コンテンツは存在しません".to_string()
+                    }
+                )
+            }
+        };
+    }
 }
