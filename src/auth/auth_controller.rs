885588647d47a6f57708c22b5d46d7a17ce846d6
kinoshita <kinoshita062323@gmail.com>
2021-10-24 03:07:38 +0900

    [change] login, registerエンドポイントを実装しました

--- src/auth/auth_controller.rs
+++ src/auth/auth_controller.rs
@@ -1,5 +1,41 @@
 
-pub async fn login() {
+use actix_web::web;
+use actix_web::Responder;
+use actix_web::HttpResponse;
+use crate::app_module::AppModule;
+use crate::user::services::LoginRequest;
+use crate::user::services::RegisterRequest;
+
+pub async fn login(data: web::Data<AppModule>, json: web::Json<LoginRequest>) -> impl Responder {
+
+    let um = data.as_ref().user_module();
+    let user_service = um.user_service();
+    match user_service.login(&json.0) {
+        Ok(res) => {
+            return HttpResponse::Ok().json(res);
+        }
+        Err(e) => {
+            return e.response();
+        }
+    }   
+
+
+
+}
+
+pub async fn register(data: web::Data<AppModule>, json: web::Json<RegisterRequest>) -> impl Responder {
+
+    let um = data.as_ref().user_module();
+    let user_service = um.user_service();
+
+    match user_service.register(&json.0) {
+        Ok(res) => {
+            return HttpResponse::Ok().json(res);
+        }
+        Err(e) => {
+            return e.response();
+        }
+    }
 
 
 }
