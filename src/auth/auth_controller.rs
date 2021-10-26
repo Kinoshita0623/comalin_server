7b083625f84f28dbaa883128dede56f57757dbb1
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-26 16:52:25 +0900

    [change] web::blockを使用するようにしました

--- src/auth/auth_controller.rs
+++ src/auth/auth_controller.rs
@@ -5,38 +5,40 @@ use actix_web::HttpResponse;
 use crate::app_module::AppModule;
 use crate::user::services::LoginRequest;
 use crate::user::services::RegisterRequest;
+use crate::errors::service_error::ServiceError;
+use crate::user::services::AuthResponse;
+
 
 pub async fn login(data: web::Data<AppModule>, json: web::Json<LoginRequest>) -> impl Responder {
 
-    let um = data.as_ref().user_module();
-    let user_service = um.user_service();
-    match user_service.login(&json.0) {
+    let be: ServiceError = match web::block(move || -> Result<AuthResponse, ServiceError>{
+        let um = data.as_ref().user_module();
+        let user_service = um.user_service();
+        return user_service.login(&json.0);
+    }).await {
         Ok(res) => {
             return HttpResponse::Ok().json(res);
-        }
-        Err(e) => {
-            return e.response();
-        }
-    }   
-
+        },
+        Err(e) => e.into()
+    };
+    return be.response();
 
 
 }
 
 pub async fn register(data: web::Data<AppModule>, json: web::Json<RegisterRequest>) -> impl Responder {
 
-    let um = data.as_ref().user_module();
-    let user_service = um.user_service();
+    let be = match web::block(move || -> Result<AuthResponse, ServiceError> { 
+        let um = data.as_ref().user_module();
+        let user_service = um.user_service();
 
-    match user_service.register(&json.0) {
+        user_service.register(&json.0)
+    } ).await {
         Ok(res) => {
             return HttpResponse::Ok().json(res);
-        }
-        Err(e) => {
-            return e.response();
-        }
-    }
-
-
+        },
+        Err(e) => e
+    };
+    let e: ServiceError = be.into();
+    return e.response();
 }
-
