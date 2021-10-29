ae3530e1cb3fb0eeffbb38ac5df9f7b9bd4ee9b2
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-30 03:01:50 +0900

    [change] repositoryを作成

---
+++ src/files/repositories.rs
@@ -0,0 +1,7 @@
+use crate::files::entities::*;
+use crate::errors::service_error::ServiceError;
+
+pub trait AppFileRepository {
+    fn create(&self, file: &NewAppFile) -> Result<AppFile, ServiceError>;
+    fn find_by_hash(&self, hash: String) -> Result<AppFile, ServiceError>;
+}
