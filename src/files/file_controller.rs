a2f28aa5eb542f9e77735ecc8a7d0bc93949f31e
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-30 00:27:30 +0900

    [change] mimetype, extenstion, capacityの取得をできるようになりました

--- src/files/file_controller.rs
+++ src/files/file_controller.rs
@@ -6,6 +6,7 @@ use uuid::Uuid;
 use actix_web::web::Buf;
 use std::io::Write;
 
+
 use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 
 pub async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error>{
@@ -14,21 +15,38 @@ pub async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error>{
         let filename = content_type.get_filename().unwrap();
         println!("filename:{}", filename);
         println!("mime:{:?}", content_type);
-        let filepath = format!("./tmp/{}", Uuid::new_v4().to_string());
+        let tmp_filename = Uuid::new_v4().to_string();
+        let filepath = format!("./tmp/{}", tmp_filename.clone());
 
         // File::create is blocking operation, use threadpool
-        let mut f: std::fs::File = web::block(|| std::fs::File::create(filepath))
+        let filepath2 = filepath.clone();
+        let mut f: std::fs::File = web::block(|| std::fs::File::create(filepath2))
             .await
             .unwrap();
         
-            
-
+        let mut cap = 0;
+        
         // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.next().await {
             let data: Bytes = chunk.unwrap();
             // filesystem operations are blocking, we have to use threadpool
+
+            cap += data.len();
             f = web::block(move || f.write_all(&data).map(|_| f)).await?;
         }
+        
+       
+        let kind = match infer::get_from_path(filepath)? {
+            Some(kind) => kind,
+            None => {
+                return Ok(HttpResponse::Ok().into());
+            }
+        };
+        let extenstion = kind.extension();
+        let mime_type = kind.mime_type();
+        
+        println!("extenstion:{}, mimetype:{}, cap:{}", extenstion, mime_type, cap);
+
     }
     return Ok(HttpResponse::Ok().into());
 }
