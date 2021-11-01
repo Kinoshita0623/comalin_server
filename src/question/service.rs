babac34219b807a0038364ee9c62401548056f57
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-01 23:01:32 +0900

    [change] impl

--- src/question/service.rs
+++ src/question/service.rs
@@ -12,7 +12,6 @@ use crate::user::entities::User;
 use crate::user::module::UserModule;
 use bigdecimal::{FromPrimitive, BigDecimal};
 use diesel::prelude::*;
-use diesel::associations::*;
 use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 use diesel::PgConnection;
 use crate::files::entities::AppFile;
@@ -35,7 +34,7 @@ pub struct CreateQuestion {
     pub file_ids: Option<Vec<Uuid>>
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 pub struct QuestionView {
     pub id: Uuid,
     pub title: String,
@@ -52,7 +51,7 @@ pub struct QuestionView {
     pub updated_at: Option<NaiveDateTime>
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 pub struct QuestionFileView {
     pub url: String,
     pub name: String
@@ -63,7 +62,7 @@ pub trait QuestionService {
 }
 
 pub struct QuestionServiceImpl {
-    pub pool: Pool<ConnectionManager<PgConnection>>,
+    pub pool: Box<Pool<ConnectionManager<PgConnection>>>,
     pub question_module: Box<dyn QuestionModule>,
     pub user_module: Box<dyn UserModule>,
 }
@@ -75,7 +74,10 @@ impl QuestionService for QuestionServiceImpl {
         let q = self.question_module.question_repository().create(&q)?;
 
         let questions = self.load_question_views_from_questions(vec![q])?;
-        return Ok(questions[0]);
+        return match questions.get(0) {
+            Some(q) => Ok(q.clone()),
+            None => return Err(ServiceError::InternalError{ body: None })
+        }
     }
 
 }
@@ -89,7 +91,7 @@ impl QuestionServiceImpl {
         let c = self.get_connection()?;
         let list: Vec<QuestionDTO> = questions.iter()
             .map(|q: &Question| -> QuestionDTO {
-                return QuestionDTO::from(*q)
+                return QuestionDTO::from(q.clone())
             })
             .collect();
 
@@ -117,8 +119,8 @@ impl QuestionServiceImpl {
         let views: Vec<QuestionView> = questions.iter().map(|q: &Question| -> QuestionView {
             return QuestionView {
                 id: q.id,
-                title: q.title,
-                text: q.text,
+                title: q.title.clone(),
+                text: q.text.clone(),
                 latitude: q.latitude.to_f64().unwrap(),
                 longitude: q.longitude.to_f64().unwrap(),
                 answers_count: q.answers_count,
@@ -126,14 +128,14 @@ impl QuestionServiceImpl {
                 files: match qf_and_af_map.get(&q.id) {
                     Some(files) => files.iter().map(|f| QuestionFileView {
                         url: "".to_string(),
-                        name: f.1.filename
+                        name: f.1.filename.clone()
                     }).collect(),
                     None => Vec::new()
                 },
-                file_ids: q.file_ids,
+                file_ids: q.file_ids.clone(),
                 created_at: q.created_at,
                 updated_at: q.updated_at,
-                user: (*users.get(&q.user_id).unwrap()).into(),
+                user: (users.get(&q.user_id).unwrap().clone()).into(),
                 user_id: q.user_id
             };
         })
@@ -161,13 +163,13 @@ impl Into<Question> for (&User, &CreateQuestion) {
     fn into(self) -> Question {
         return Question {
             id: Uuid::new_v4(),
-            title: self.1.title.unwrap(),
-            text: self.1.text,
+            title: self.1.title.clone().unwrap(),
+            text: self.1.text.clone(),
             latitude: BigDecimal::from_f64(self.1.latitude.unwrap()).unwrap(),
             longitude: BigDecimal::from_f64(self.1.longitude.unwrap()).unwrap(),
             address_id: None,
             answers_count: 0,
-            file_ids: self.1.file_ids.unwrap(),
+            file_ids: self.1.file_ids.clone().unwrap(),
             user_id: self.0.id,
             created_at: None,
             updated_at: None
@@ -175,17 +177,17 @@ impl Into<Question> for (&User, &CreateQuestion) {
     }
 }
 
-fn group_by<K, V, F>(list: Vec<V>, f: F) -> HashMap<K, Vec<V>> where F: FnMut(V) -> K, K: Eq + Hash {
+fn group_by<K, V, F>(list: Vec<V>, mut f: F) -> HashMap<K, Vec<V>> where F: FnMut(V) -> K, K: Eq + Hash, V: Clone {
     let mut map: HashMap<K, Vec<V>> = HashMap::new();
 
     for el in list {
-        let k: K = f(el);
-        let mut list: &Vec<V> = match map.get_mut(&k) {
-            Some(list) => list,
-            None => &Vec::new()
+        let k: K = f(el.clone());
+        let mut list: Vec<V> = match map.get_mut(&k) {
+            Some(list) => list.clone(),
+            None => Vec::new()
         };
-        list.push(el);
-        map.insert(k, *list);
+        list.push(el.clone());
+        map.insert(k, list.clone());
     }
     return map;
 }
